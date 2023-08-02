fe0169d61e28d12619b7a780bca972820e8bb515
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-08-02 19:04:46 +0600

    loading issues after deploy[7]

--- src/app/blog/loading.js
+++ src/app/blog/loading.js
@@ -1,12 +1,42 @@
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
+import styles from "./blog.module.css";
+
+const getData = async () => {
+  const apiUrl = process.env.API_URL;
+
+  //Fetching data to get the length
+  const res = await fetch(`${apiUrl}/api/posts`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  return res.json();
+};
+
+export default async function Loading() {
+  const data = await getData();
+
   return (
-    <div>
-      {[...Array(10).keys()].map((i) => (
+    <div className={styles.mainLoadingContainer}>
+      {[...Array(data.length).keys()].map((i) => (
         <div
-          key={i}
-          style={{ height: "200px", width: "500px", backgroundColor: "#999" }}
-        ></div>
+          key={i._id}
+          className={styles.loadingContainer}
+          style={{
+            animationDelay: `${i * 0.05}s`,
+          }}
+        >
+          <div className={styles.loadingImgContainer}>
+            <div className={styles.loadingImg} />
+          </div>
+
+          <div className={styles.loadingContent}>
+            <h1 className={styles.loadingTitle}></h1>
+            <p className={styles.loadingDesc}></p>
+          </div>
+        </div>
       ))}
     </div>
   );
