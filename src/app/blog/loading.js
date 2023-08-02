18d2613343b35beef50a5be5f08eb970b3b7acc7
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-08-02 19:46:12 +0600

    loading issues after deploy[solved] trying with data again

--- src/app/blog/loading.js
+++ src/app/blog/loading.js
@@ -1,10 +1,29 @@
 import styles from "./blogLoading.module.css";
 
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
+const getData = async () => {
+  const apiUrl = process.env.API_URL;
+
+  //Fetching data to get data length
+  const res = await fetch(`${apiUrl}/api/posts`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  return res.json();
+};
+
+export default async function Loading() {
+  //this is just to add that after 11 tries, the issue of adding the loader is solved and cause of it was importing the same css file as the blog. always gonna create a different css file from now on if using plain css.
+  //now will try to fetch data to get the length and showing loader according to that, hopefully the issue doesn't happen again
+
+  const data = await getData();
+
   return (
     <div className={styles.mainLoadingContainer}>
-      {[...Array(10).keys()].map((i) => (
+      {[...Array(data.length).keys()].map((i) => (
         <div
           key={i}
           className={styles.loadingContainer}
