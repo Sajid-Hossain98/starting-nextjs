fef6033878cbf4b02c91043dd68851f1fffe88ab
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-08-02 18:57:48 +0600

    loading issues after deploy[6]

--- src/app/blog/loading.js
+++ src/app/blog/loading.js
@@ -1,4 +1,13 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <p>Loading...</p>;
+  return (
+    <div>
+      {[...Array(10).keys()].map((i) => (
+        <div
+          key={i}
+          style={{ height: "200px", width: "500px", backgroundColor: "#999" }}
+        ></div>
+      ))}
+    </div>
+  );
 }
