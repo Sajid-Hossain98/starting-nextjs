7c9e3096187f8333d1eb597f6fe822b5eb61ec80
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-09-06 13:26:12 +0600

    link to register page and register page responsive

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import styles from "./login.module.css";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Login = () => {
   const session = useSession();
@@ -43,7 +44,11 @@ const Login = () => {
           className={styles.input}
         />
         <button className={styles.button}>Login</button>
+        <Link href="/dashboard/register" className={styles.createNewAccount}>
+          Create an account
+        </Link>
       </form>
+
       <button
         onClick={() => signIn("google")}
         className={styles.loginWithGoogle}
